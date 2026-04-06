d3ddadb4a6bb9b2e387f531e5ce5310c1b22f0e8
saldivarline <evelinesaldivar@gmail.com>
2026-04-06 10:42:33 +0800

    fix deployment bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Route } from 'react-router-dom';
+import React from 'react';
+import { Route } from 'react-router-dom';         
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
@@ -18,19 +19,12 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
+/* Ionic Dark Mode */
 import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
-import './theme/variables.css';
+import './theme/variables.css';                   
+
 import Login from './pages/Login';
 import Menu from './pages/Menu';
 
@@ -40,10 +34,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-
-        <Route exact path="/" component={Login}></Route>
-        <Route path="/app" component={Menu}></Route>
-        
+        <Route exact path="/" component={Login} />
+        <Route path="/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
